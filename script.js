65c9e5b9c353e5c59150ec41bb869c88fec89a32
Soham-gx <gg0240521@gmail.com>
2025-01-22 13:24:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function navigateToLogin() {
-    window.location.href = "login.html"; // Redirect to login or sign up page
+    window.location.href = "https://script.google.com/macros/s/AKfycbzbZVgnxJJArIbLrPW1zehhHfz6z7u8m_KpbbnF-EApBEyxc-HM6Q8dUUIwjLU5QDmB/exec"; // Redirect to login or sign up page
 }
