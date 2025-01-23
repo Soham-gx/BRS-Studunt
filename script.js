c50177e11043ac3055b5a6689456dd6cc0e7eb21
Soham-gx <gg0240521@gmail.com>
2025-01-23 12:29:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function navigateToLogin() {
-    window.location.href = "https://script.google.com/macros/s/AKfycbzbZVgnxJJArIbLrPW1zehhHfz6z7u8m_KpbbnF-EApBEyxc-HM6Q8dUUIwjLU5QDmB/exec"; // Redirect to login or sign up page
-}
