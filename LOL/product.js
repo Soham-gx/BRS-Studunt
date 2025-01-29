6533c14509bafc04415c46f2257fad29d41df504
Soham-gx <gg0240521@gmail.com>
2025-01-29 16:00:45 +0530

    Create product.js

---
+++ LOL/product.js
@@ -0,0 +1,7 @@
+// Add to Cart Function
+function addToCart(product) {
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.push(product);
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert('Product added to cart!');
+}
